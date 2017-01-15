1d771c0fc5d8b531a2197ae6b9480b485d554cb0
Yuki Tsuji <yukitsuji020832@gmail.com>
2017-01-15 23:27:49 +0900

    Update model.py

--- model.py
+++ model.py
@@ -6,51 +6,29 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Dense, Activation, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers.normalization import BatchNormalization
 from keras.optimizers import Adam
-from keras.utils import np_utils
 from keras import backend as K
 from keras.models import model_from_json
 import cv2
-import matplotlib.pyplot as plt
 from PIL import Image
-from io import BytesIO
-from keras.preprocessing.image import img_to_array, load_img
 
-def input_rgb(image_path):
-    image = Image.open(image_path)
-    image = np.asarray(image)
-    return image
-
-def flip_image(image):
-    return cv2.flip(image, 1)
-
-def augment_brightness_camera_image(image):
-    image1 = cv2.cvtColor(image,cv2.COLOR_RGB2HSV)
-    random_bright = .25+np.random.uniform()
-    image1[:,:,2] = image1[:,:,2]*random_bright
-    image1 = cv2.cvtColor(image1,cv2.COLOR_HSV2RGB)
-    return image1
-
-def save_image_from_numpy(image, path):
-    image = Image.fromarray(np.uint8(image))
-    image.save(path)
-
-def augmentation_datasets(images_path, steering_sets, add_steering=0.25, image_shape=(160, 320, 3), place='center', folder_path='./data/', new_data_path="./data/new_driving_log.csv"):
+def augmentated_datasets(images_path, steering_sets, add_steering=0.25, image_shape=(160, 320, 3), place='center', folder_path='./data/', new_data_path="./data/new_driving_log.csv"):
+    """sample of augment datasets"""
     with open(new_data_path, 'a') as f:
         steering_sets += add_steering
-        
+
         df = pd.DataFrame({
             'image' : images_path,
-            'steering' : steering_sets,  
+            'steering' : steering_sets,
         })
         if place == 'center':
             df.to_csv(f, header=True)
         else:
             df.to_csv(f, header=False)
-            
+
         for name in ["FLIP_", "BRIGHT_", "FLIP_BRIGHT_"]:
             path = name + images_path
             df["image"] = path
@@ -59,11 +37,11 @@ def augmentation_datasets(images_path, steering_sets, add_steering=0.25, image_s
             else:
                 df["steering"] = steering_sets * -1
             df.to_csv(f, header=False)
-            
+
             dir_path = folder_path + name + "IMG"
             if not os.path.exists(dir_path):
                 os.mkdir(dir_path)
-        
+
         for index, image_path in enumerate(images_path):
             image_path = image_path.lstrip()
             image = input_rgb(folder_path + image_path).astype(np.uint8)
@@ -74,24 +52,57 @@ def augmentation_datasets(images_path, steering_sets, add_steering=0.25, image_s
             bright_image = augment_brightness_camera_image(image)
             path = folder_path + "BRIGHT_" + image_path
             save_image_from_numpy(bright_image, path)
-            
+
             fliped_bright_image = augment_brightness_camera_image(fliped_image)
             path = folder_path + "FLIP_BRIGHT_" + image_path
-            save_image_from_numpy(fliped_bright_image, path)   
-            
+            save_image_from_numpy(fliped_bright_image, path)
+
 def create_datasets(sample_data):
+    """create new datasets by augment datasets"""
     NEW_DATA_PATH = "./data/new_driving_log.csv"
-    
+
     if os.path.exists(NEW_DATA_PATH):
         os.remove(NEW_DATA_PATH)
-        
+
     steering_sets = sample_data['steering']
 
-    augmentation_datasets(sample_data['center'], steering_sets, add_steering=0, new_data_path=NEW_DATA_PATH)
-    augmentation_datasets(sample_data['left'], steering_sets, add_steering=0.25, new_data_path=NEW_DATA_PATH, place='left')
-    augmentation_datasets(sample_data['right'], steering_sets, add_steering=-0.25, new_data_path=NEW_DATA_PATH, place='right')
-    
+    augmentated_datasets(sample_data['center'], steering_sets, add_steering=0, new_data_path=NEW_DATA_PATH)
+    augmentated_datasets(sample_data['left'], steering_sets, add_steering=0.25, new_data_path=NEW_DATA_PATH, place='left')
+    augmentated_datasets(sample_data['right'], steering_sets, add_steering=-0.25, new_data_path=NEW_DATA_PATH, place='right')
+
+def input_rgb(image_path):
+    """Returns: numpy array of RGB Image"""
+    image = Image.open(image_path)
+    image = np.asarray(image)
+    return image
+
+def flip_image(image):
+    """Horizontal Flip"""
+    return cv2.flip(image, 1)
+
+def augment_brightness_camera_image(image):
+    """changing brightness of images."""
+    image1 = cv2.cvtColor(image,cv2.COLOR_RGB2HSV)
+    random_bright = .25+np.random.uniform()
+    image1[:,:,2] = image1[:,:,2]*random_bright
+    image1 = cv2.cvtColor(image1,cv2.COLOR_HSV2RGB)
+    return image1
+
+def save_image_from_numpy(image, path):
+    """save RGB image"""
+    image = Image.fromarray(np.uint8(image))
+    image.save(path)
+
 def get_model_architecture(init='glorot_uniform', input_shape=(160, 320, 3)):
+    """Define Model Architecture.
+
+    Args:
+        init (string): the method of Initializing parameter of Convolution layers
+        input_shape (tuple): input shape of First Layer
+
+    Returns:
+        model: defined model
+    """
     model = Sequential()
     model.add(Conv2D(32, 5, 5, input_shape=input_shape, init=init, border_mode='same'))
     model.add(Activation('relu'))
@@ -109,7 +120,7 @@ def get_model_architecture(init='glorot_uniform', input_shape=(160, 320, 3)):
     model.add(Dense(128, activation='relu'))
     model.add(BatchNormalization())
     model.add(Dense(1))
-    
+
     model.compile(
         optimizer='adam',
         loss='mean_squared_error',
@@ -118,6 +129,17 @@ def get_model_architecture(init='glorot_uniform', input_shape=(160, 320, 3)):
     return model
 
 def preprocessing(rgb_image, input_shape=(160, 320)):
+    """Preprocess image.
+    1. normalization
+    2. crop and resize
+
+    Args:
+        rgb_image (numpy array): 3 dimensional Numpy array of RGB image
+        input_shape (tuple): shape of resized image (x, y)
+
+    Returns:
+        rgb_image: preprocessed Image
+    """
     #normalization
     rgb_image = rgb_image / rgb_image.max() - 0.5
     rgb_image = rgb_image[55:135, :, :]
@@ -125,28 +147,53 @@ def preprocessing(rgb_image, input_shape=(160, 320)):
     return rgb_image
 
 def augmentation_image(row, input_shape=(160, 320)):
+    """Image Augmentation
+    1. select camera place
+    2. select flip or not
+    3. changing brightness of image
+
+    Args:
+        row (pandas DataFrame): image path of each camera place, and steering value.
+        input_shape (tuple): shape of image (x, y) dimension.
+
+    Returns:
+        image (numpy array): augmented image
+        steering (float): steering corresponding to image augmentation
+    """
     steering = row['steering']
     # choose camera's place
     place = choice(["center", "left", "right"])
     image = input_rgb("./data/" + row[place]).astype(np.float32)
-    
+
     if place == "left":
         steering += 0.25
     elif place == "right":
         steering -= 0.25
-        
+
     # flip or not
-    rd = random()
     if random() < 0.5:
         image = flip_image(image)
         steering = steering * -1
-    
+
     # brightness
     image = augment_brightness_camera_image(image)
-    
+
     return image, steering
-    
+
 def generate_arrays_from_datasets(datasets, batch_size=128, input_shape=(160, 320)):
+    """generator to read data from image path, augment on the fly and use it to train model.
+
+    args:
+        datasets (pandas dataframe): datasets of training, validation, test.
+                                     including image path of each place, and steering
+        batch_size (int): batch size of training.
+        input_shape (tuple): input shape of image (x, y) dimension
+
+    Returns:
+        image_batch (numpy array): 4 dimensional array (batch_size, x, y, 3)
+                                   1 batch of augmented and preprocessed images.
+        steering_batch (numpy array): 1 dimensional array. steering sets of 1 batch
+    """
     epoch_num = datasets.shape[0] // batch_size
     while True:
         for epoch in range(epoch_num): # one batch
@@ -157,7 +204,7 @@ def generate_arrays_from_datasets(datasets, batch_size=128, input_shape=(160, 32
                 image, steering = augmentation_image(row)
                 image_batch[index], steering_batch[index] = preprocessing(image, input_shape=input_shape), steering
             yield (image_batch, steering_batch)
-    
+
 if __name__ == '__main__':
     np.random.seed(0)
     try:
@@ -186,16 +233,15 @@ if __name__ == '__main__':
 
         samples_per_epoch = training_sets.shape[0] // BATCH_SIZE * BATCH_SIZE
         nb_val_samples = validation_sets.shape[0] // BATCH_SIZE * BATCH_SIZE
-        
+
         model.fit_generator(
             training_generator, samples_per_epoch=samples_per_epoch, nb_epoch=EPOCH,
             validation_data=validation_generator, nb_val_samples=nb_val_samples
         )
-        
-        model.save_weights('model_conv3_aug_24_64.hdf5')
+
+        model.save_weights('model.h5')
         json_string = model.to_json()
 
-        with open('model_conv3_aug_24_64.json', 'w') as f:
+        with open('model.json', 'w') as f:
             f.write(json_string)
         K.clear_session()
-
